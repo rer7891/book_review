9227b8d683690f1ecc10ff3005d6d82e8618180f
Becky Robran <rerobran@gmail.com>
2020-02-21 10:34:30 -0700

    updates test for reviews

--- spec/features/user_can_see_welcome_page_spec.rb
+++ spec/features/user_can_see_welcome_page_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
-
+#
+# I should also see:
+# - This book has 2 reviews from the New York Times
+#   (Note: reviews are the "summary" in the book review response)
+# - Review 1: ""The Man Who Saw Everything,"" which was longlisted for the Booker Prize, looks at masculinity through the perspective of a young historian who sneers at "authoritarian old men.""
+# Review Publication Date: 2019-10-09
+# - Review 2: "Deborah Levy's latest novel, "The Man Who Saw Everything," experiments with time travel, history and the endless complications of love."
+# Review Publication Date: 2019-10-15
 describe('User can visit the welcome page') do
   it('and see a book search form') do
     visit root_path
@@ -15,5 +22,13 @@ describe('User can visit the welcome page') do
     expect(page).to have_css(".title")
     expect(page).to have_css(".author")
     expect(page).to have_css(".genres")
+
+    expect(page).to have_css(".reviews", count: 2)
+
+      within(first(".reviews")) do
+        expect(page).to have_content("Review 1")
+        expect(page).to have_content("Normal People")
+        expect(page).to have_content("Review Publication Date")
+      end
   end
 end
