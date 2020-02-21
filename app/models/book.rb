ab8bf48955a56fc696228b780054f19517d1872a
Becky Robran <rerobran@gmail.com>
2020-02-21 10:31:51 -0700

    takes out pry

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,6 @@
 class Book
   attr_reader :title, :author, :genre
   def initialize(data)
-require "pry"; binding.pry
     @title = data[:docs].first[:title_suggest]
     @author = data[:docs].first[:author_name]
     @genre = data[:docs][2][:subject]
