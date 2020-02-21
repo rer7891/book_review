a6b824ef7cedf81ad844f666c740725e2f34f7c4
Becky Robran <rerobran@gmail.com>
2020-02-21 11:07:29 -0700

    updates services to have private methods

--- app/services/book_service.rb
+++ app/services/book_service.rb
@@ -1,13 +1,15 @@
 class BookService
 
+  def get_book(title)
+    response = conn.get("/search.json?title=#{title}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+private
+
   def conn
     Faraday.new(url: 'http://openlibrary.org') do |faraday|
       faraday.adapter Faraday.default_adapter
     end
-  end 
-
-  def get_book(title)
-    response = conn.get("/search.json?title=#{title}")
-    JSON.parse(response.body, symbolize_names: true)
   end
 end
--- app/services/review_service.rb
+++ app/services/review_service.rb
@@ -1,13 +1,16 @@
 class ReviewService
-  def conn
-    Faraday.new(url: 'http://api.nytimes.com') do |faraday|
-      faraday.adapter Faraday.default_adapter
-    end
-  end
 
   def get_reviews(title)
     key = ENV['NY_API_KEY']
     response = conn.get("/svc/books/v3/reviews.json?api-key=#{key}&title=#{title}")
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: 'http://api.nytimes.com') do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
   end
 end
