520d2f194f06f88d38dc592458380a46d1149d90
Becky Robran <rerobran@gmail.com>
2020-02-21 09:23:42 -0700

    updates test

--- spec/features/user_can_see_welcome_page_spec.rb
+++ spec/features/user_can_see_welcome_page_spec.rb
@@ -7,5 +7,12 @@ describe('User can visit the welcome page') do
     expect(page).to have_content("Welcome to Book Review")
     expect(page).to have_content("Search for a Book")
     expect(page).to have_button("Find Book")
+
+    fill_in "Title", with: "the man who saw everything"
+    click_on "Find Book"
+    expect(current_path).to eq(search_path)
+    expect(page).to have_css(".title")
+    expect(page).to have_css(".author")
+    expect(page).to have_css(".genres")
   end
 end
