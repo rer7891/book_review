0366998bf8745f9dcac1519b36cdb4215d19b8d4
Becky Robran <rerobran@gmail.com>
2020-02-21 11:17:17 -0700

    takes out unwanted text

--- spec/features/user_can_see_welcome_page_spec.rb
+++ spec/features/user_can_see_welcome_page_spec.rb
@@ -1,12 +1,5 @@
 require 'rails_helper'
-#
-# I should also see:
-# - This book has 2 reviews from the New York Times
-#   (Note: reviews are the "summary" in the book review response)
-# - Review 1: ""The Man Who Saw Everything,"" which was longlisted for the Booker Prize, looks at masculinity through the perspective of a young historian who sneers at "authoritarian old men.""
-# Review Publication Date: 2019-10-09
-# - Review 2: "Deborah Levy's latest novel, "The Man Who Saw Everything," experiments with time travel, history and the endless complications of love."
-# Review Publication Date: 2019-10-15
+
 describe('User can visit the welcome page') do
   it('and see a book search form') do
     visit root_path
