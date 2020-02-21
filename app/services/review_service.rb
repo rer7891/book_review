2e59bcec198505def623790308c631a327b5655c
Becky Robran <rerobran@gmail.com>
2020-02-21 10:57:41 -0700

    adds api call to ny times

--- app/services/review_service.rb
+++ app/services/review_service.rb
@@ -1,2 +1,13 @@
 class ReviewService
-end 
+  def conn
+    Faraday.new(url: 'http://api.nytimes.com') do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_reviews(title)
+    key = ENV['NY_API_KEY']
+    response = conn.get("/svc/books/v3/reviews.json?api-key=#{key}&title=#{title}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
